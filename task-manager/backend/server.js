2ac8814d21b85db38ef3ac484ebdd85afdc9befd
Tal Zigelnik <zigelnik@gmail.com>
2025-10-16 12:34:24 +0300

    expose ports

--- task-manager/backend/server.js
+++ task-manager/backend/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
