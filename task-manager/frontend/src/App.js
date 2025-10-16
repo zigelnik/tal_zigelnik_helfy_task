ad76fbf215809d9cc31320f7afca57a7d207af6d
Tal Zigelnik <zigelnik@gmail.com>
2025-10-16 11:59:34 +0300

    react app init and gitignore

---
+++ task-manager/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Task Manager</h1>
+        <p>Welcome to your minimal React application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
